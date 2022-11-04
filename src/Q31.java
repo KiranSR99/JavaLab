09ad5007ee47f8ab5caad5f2dfe272775009be89
KiranSR99 <kiranxtha99@gmail.com>
2022-11-04 09:01:34 +0545

    first commit

---
+++ src/Q31.java
@@ -0,0 +1,19 @@
+import  java.util.Scanner;
+public class Q31 {
+    public static void main(String[] args) {
+        System.out.print("How many elements you want on array ? ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[50];
+        System.out.println("Enter the elements in array: ");
+
+        for (int i=0; i<n; i++){
+            System.out.print("Element "+(i+1)+": ");
+            array[i] = sc.nextInt();
+        }
+        System.out.println("\nElements of array are: ");
+        for (int i=0; i<n; i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+}
