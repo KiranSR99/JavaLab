4ace876dd3f6a32387f08a129c99ac6ffc2b9f07
Kiran Shrestha <kiranxtha99@gmail.com>
2022-11-24 22:44:57 +0545

    Q32 added and changed

--- src/Q32.java
+++ src/Q32.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 public class Q32 {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6};
+        int[] arr = {1,2,3,4,5,6};
         int n = arr.length;
-        int newArr[] = new int[n+1];
+        int[] newArr = new int[n+1];
         int value = 7;
         System.out.println(Arrays.toString(arr));
         for(int i = 0; i<n; i++) {
