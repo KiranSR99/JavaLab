7723a418ee207b49287bd3681a8c1d45c22f4073
Kiran Shrestha <kiranxtha99@gmail.com>
2022-11-26 23:58:22 +0545

    changes made on Q40 file

--- src/Q40.java
+++ src/Q40.java
@@ -1,8 +1,8 @@
 public class Q40 {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
-        int a[] = new int[2];
+        int[] a = new int[2];
         try {
             System.out.println("Access element three :" + a[3]);
         } catch (ArrayIndexOutOfBoundsException e) {
