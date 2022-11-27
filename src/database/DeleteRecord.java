24d415bbbd992c8813b5807c51cc0354c49cc140
Kiran Shrestha <kiranxtha99@gmail.com>
2022-11-27 17:27:20 +0545

    changes made

--- src/database/DeleteRecord.java
+++ src/database/DeleteRecord.java
@@ -1,7 +1,7 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+//import java.sql.Connection;
+import java.sql.*;
 import java.util.Scanner;
 
 public class DeleteRecord extends DatabaseConnect{
